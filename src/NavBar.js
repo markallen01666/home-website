66bcf8df94dea9b7fa26be92099b76dc612d5ee5
Mark Allen <markallen.webdeveloper@gmail.com>
2019-11-05 11:43:39 +0000

    Minor mods including page title and adding my name to NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,6 +7,7 @@ class NavBar extends Component {
   render() {
     return (
       <div className="App-navbar">
+        <h1>Mark Allen</h1>
         <ul>
           <li>Home</li>
           <li>Services</li>
